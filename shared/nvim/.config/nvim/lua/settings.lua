0c35ff0d0b532bdf1f2986490b7a4a346d85f47d
Anderson Joseph <andersonjoseph@mailfence.com>
2025-04-16 23:48:10 -0400

    add some general goodies

--- shared/nvim/.config/nvim/lua/settings.lua
+++ shared/nvim/.config/nvim/lua/settings.lua
@@ -6,3 +6,10 @@ vim.opt.ignorecase = true
 vim.opt.mouse = ""
 vim.opt.number = true
 vim.opt.relativenumber = true
+
+vim.opt.scrolloff = 8  -- Start scrolling when 8 lines from edge
+vim.opt.undofile = true  -- Persistent undo history
+vim.opt.smartindent = true  -- Smart indentation 
+vim.opt.cursorline = true  -- Highlight current line
+vim.opt.splitright = true  -- Put new windows right of current
+vim.opt.splitbelow = true  -- Put new windows below current
